050ff6487ae32b7aa64f213756036584c4c6c405
faiq <faiqrazarizvi@gmail.com>
2018-03-03 16:03:47 -0800

    Start doing with vagrant

--- poolmanager/server/cp/vbox.go
+++ poolmanager/server/cp/vbox.go
@@ -3,17 +3,20 @@ package cp
 import (
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 	"os/exec"
 
 	"github.com/google/uuid"
 )
 
-const vboxNamePrefix = "fn-virtualbox"
+const vboxNamePrefix = "fn-vagrant"
 
 var whichVBox *exec.Cmd
 
 func init() {
-	whichVBox = exec.Command("which", "vbox")
+	whichVBox = exec.Command("which", "vagrant")
 }
 
 type VirtualBoxCP struct{}
@@ -26,12 +29,35 @@ func NewVirtualBoxCP() (*VirtualBoxCP, error) {
 }
 
 func (v *VirtualBoxCP) provision() error {
-	name := newNodeName()
-	vboxProvision := exec.Command("vbox", "manage", "createvm", "--name", name, "--ostype", "Linux", "--register")
-	err := vboxProvision.Run()
+	//set up dir
+	wd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
+
+	node := newNodeName()
+	nodeDir, err := ioutil.TempDir(wd, node)
+	if err != nil {
+		return err
+	}
+	//copy vagrant file into there
+	vagrantFile := fmt.Sprintf("%s/%s", wd, "Vagrantfile")
+	newVagrantFile := fmt.Sprintf("%s/%s", nodeDir, "Vagrantfile")
+	err = copyFile(vagrantFile, newVagrantFile)
+	if err != nil {
+		return err
+	}
+
+	err = os.Chdir(nodeDir)
+	if err != nil {
+		return err
+	}
+	vboxProvision := exec.Command("vagrant", "up")
+	err = vboxProvision.Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (v *VirtualBoxCP) GetLBGRunners(lgbId string) ([]*Runner, error) {
@@ -49,5 +75,30 @@ func (v *VirtualBoxCP) RemoveRunner(lbgId string, id string) error {
 
 func newNodeName() string {
 	id := uuid.New()
-	return fmt.Sprintf("%s-%s", vagrantPrefix, id.String())
+	return fmt.Sprintf("%s-%s", vboxNamePrefix, id.String())
+}
+
+func copyFile(src string, dst string) error {
+	// Open the source file for reading
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	// Open the destination file for writing
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	// Copy the contents of the source file into the destination file
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+
+	// Return any errors that result from closing the destination file
+	// Will return nil if no errors occurred
+	return d.Close()
 }
